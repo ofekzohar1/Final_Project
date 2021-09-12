f81eae8888adcaaf3af7b9ced92aac3a347d18bb
Ofek Zohar <ofekzohar1@mail.tau.ac.il>
2021-09-12 11:51:44 +0300

    spkmeansmodule doc

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -1,45 +1,30 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#include "spkmeans.h"
+#include "spkmeansmodule.h" /* Macros and functions declarations */
+#include "spkmeans.h" /* spk's public functions and macros */
 
-#define MyAssert(exp) \
-if (!(exp)) {         \
-freeAllMemory();                      \
-return NULL;   \
-}
+/**********************************
+********* Module settings *********
+**********************************/
 
-/* Functions declaration */
-static PyObject *calc_mat_connect(PyObject *self, PyObject *args);
-static PyObject *kmeans_connect(PyObject *self, PyObject *args);
-static PyObject *jacobi_connect(PyObject *self, PyObject *args);
-PyObject *cMatToPyLOL(double **matrix, int rows, int cols);
-double **pyLOLToCMat(PyObject *pyListOfLists, int rows, int cols);
-int *pyIntListToCArray(PyObject *pyIntList, int len);
-PyObject *cArrToPythonList(double *array, int len);
-PyObject *kmeansResToPyObject(double **matrix, int rows, int cols, int numOfDatapoints);
-PyObject *jacobiResToPyObject(double **eigenvectorsMat, double *eigenvalues, int n);
+/* This array tells Python what methods this module has */
+static PyMethodDef method[] = {
+        {"calc_mat", /* the Python method name that will be used */
+         (PyCFunction) calc_mat_connect,
+         /* the C-function that implements the Python function and returns static PyObject*  */
+         METH_VARARGS, /* flags indicating parameters are accepted for this function */
+         /*  The docstring for the function (PyDoc_STR("")) */
+         PyDoc_STR("Return calculated matrix (wMat/ddgMat/Lnorm/tMat) "
+                   "according to the goal provided.\n Spk goal returns tMat.")},
 
-/*
- * This array tells Python what methods this module has.
- * We will use it in the next structure
- */
-static PyMethodDef _method[] = {
-        {"calc_mat",                      /* the Python method name that will be used */
-         (PyCFunction) calc_mat_connect, /* the C-function that implements the Python function and returns static PyObject*  */
-         METH_VARARGS,   /* flags indicating parametersaccepted for this function */
-         PyDoc_STR("Return calculated matrix (wMat/ddgMat/Lnorm/tMat) according to the goal provided."
-                   "\n Spk goal returns tMat.")},      /*  The docstring for the function (PyDoc_STR("")) */
-        {"jacobi",                      /* the Python method name that will be used */
-         (PyCFunction) jacobi_connect, /* the C-function that implements the Python function and returns static PyObject*  */
-         METH_VARARGS,   /* flags indicating parametersaccepted for this function */
+        {"jacobi", (PyCFunction) jacobi_connect, METH_VARARGS,
          PyDoc_STR("Run Jacobi's algorithm on a symmetric matrix."
-                   "\nReturn the eigenvectors matrix and list of eigenvalues.")},      /*  The docstring for the function (PyDoc_STR("")) */
-        {"kmeans",                      /* the Python method name that will be used */
-         (PyCFunction) kmeans_connect, /* the C-function that implements the Python function and returns static PyObject*  */
-         METH_VARARGS,   /* flags indicating parametersaccepted for this function */
-         PyDoc_STR("Run KMeans's algorithm. Return the final centroids and vectors labeling.")},      /*  The docstring for the function (PyDoc_STR("")) */
-         {NULL, NULL, 0, NULL}        /* The is a sentinel. Python looks for this entry to know that all
-                                       of the functions for the module have been defined. */
+                   "\nReturn the eigenvectors matrix and list of eigenvalues.")},
+
+        {"kmeans", (PyCFunction) kmeans_connect, METH_VARARGS,
+         PyDoc_STR("Run KMeans algorithm. Return the final centroids and vectors labeling.")},
+
+         {NULL, NULL, 0, NULL} /* This is a sentinel */
 };
 
 /* This initiates the module using the above definitions. */
@@ -47,14 +32,16 @@ static struct PyModuleDef moduledef = {
         PyModuleDef_HEAD_INIT,
         "spkmeans", /* name of module */
         NULL, /* module documentation, may be NULL */
-        -1,  /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
-        _method /* the PyMethodDef array from before containing the methods of the extension */
+        -1,  /* size of per-interpreter state of the module,
+                    * or -1 if the module keeps state in global variables. */
+        method /* the PyMethodDef array from before containing the methods of the extension */
 };
 
 /*
- * The PyModuleDef structure, in turn, must be passed to the interpreter in the module’s initialization function.
- * The initialization function must be named PyInit_name(), where name is the name of the module and should match
- * what we wrote in struct PyModuleDef.
+ * The PyModuleDef structure, in turn, must be passed to the interpreter in the
+ *      module’s initialization function.
+ * The initialization function must be named PyInit_name(), where name is the
+ *      name of the module and should match what we wrote in struct PyModuleDef.
  * This should be the only non-static item defined in the module file
  */
 PyMODINIT_FUNC
@@ -62,89 +49,111 @@ PyInit_spkmeans(void) {
     return PyModule_Create(&moduledef);
 }
 
-/*
-* This actually defines the kMeans function using a wrapper C API function
-* The wrapping function needs a PyObject* self argument.
-* This is a requirement for all functions and methods in the C API.
-* It has input PyObject *args from Python.
-*/
+/**********************************
+******** Connect functions ********
+**********************************/
+
+/* The C-function that implements the Python function calc_mat. */
 static PyObject *calc_mat_connect(PyObject *self, PyObject *args) {
     PyObject *pyListOfLists, *pyResult;
     int k, dimension, numOfDatapoints, cols;
     double **datapointsArray, **calcMat;
     char *strGoal;
     GOAL goal;
-    headOfMemList= NULL, freeUsedMem = NULL;
+    headOfMemList= NULL, freeUsedMem = NULL; /* Init C memory containers */
 
-    MyAssert(PyArg_ParseTuple(args, "Osiii",&pyListOfLists, &strGoal, &k, &dimension, &numOfDatapoints));
-    /* Type error - not in correct format */
+    MyAssert(PyArg_ParseTuple(args, "Osiii", &pyListOfLists, &strGoal, &k,
+                              &dimension, &numOfDatapoints));
+    /* Assert fail == Type error - not in correct format */
 
     goal = str2enum(strGoal);
-    MyAssert(goal != NUM_OF_GOALS); /* Valid goal */
+    if (goal == NUM_OF_GOALS) { /* Not Valid goal */
+        PyErr_SetString(PyExc_ValueError, "Not valid goal.");
+        return NULL;
+    }
+    /* Convert python matrix to C matrix */
     datapointsArray = pyLOLToCMat(pyListOfLists, numOfDatapoints, dimension);
     MyAssert(datapointsArray != NULL);
-    calcMat = dataAdjustmentMatrices(datapointsArray, goal, &k, dimension, numOfDatapoints);
+    /* Calc matrix according to the goal provided */
+    calcMat = dataAdjustmentMatrices(datapointsArray, goal, &k, dimension,
+                                     numOfDatapoints);
     MyAssert(calcMat != NULL);
+
     if (goal == spk)
-        cols = k;
+        cols = k; /* T's dimensions - N x K */
     else
-        cols = numOfDatapoints;
+        cols = numOfDatapoints; /* Otherwise - N x N */
+    /* Convert result back to python type List of lists */
     pyResult = cMatToPyLOL(calcMat, numOfDatapoints, cols);
 
     freeAllMemory();
     return pyResult;
 }
-/*
-* This connects the kMeans function using a wrapper C API function
-* The wrapping function needs a PyObject* self argument.
-* It has input PyObject *args from Python.
-*/
+
+/* The C-function that implements the Python function kmeans. */
 static PyObject *kmeans_connect(PyObject *self, PyObject *args) {
     PyObject *pyListOfLists, *pyResult, *pyListOfIndexes;
     int k, dimension, numOfDatapoints, *firstCentralIndexes;
     double **datapointsArray, **calcMat;
-    headOfMemList= NULL, freeUsedMem = NULL;
+    headOfMemList= NULL, freeUsedMem = NULL; /* Init C memory containers */
 
-    MyAssert(PyArg_ParseTuple(args, "OiiiO",&pyListOfLists, &numOfDatapoints, &dimension, &k, &pyListOfIndexes));
-    /* Type error - not in correct format */
+    MyAssert(PyArg_ParseTuple(args, "OiiiO",&pyListOfLists, &numOfDatapoints,
+                              &dimension, &k, &pyListOfIndexes));
+    /* Assert fail == Type error - not in correct format */
 
+    /* Convert python types to C types */
     datapointsArray = pyLOLToCMat(pyListOfLists, numOfDatapoints, dimension);
     firstCentralIndexes = pyIntListToCArray(pyListOfIndexes, k);
     MyAssert(datapointsArray != NULL && firstCentralIndexes != NULL);
-    calcMat = kMeans(datapointsArray, numOfDatapoints, dimension, k, firstCentralIndexes, MAX_KMEANS_ITER);
+    /* KMeans clustering using 'kmeans' implementation in C */
+    calcMat = kMeans(datapointsArray, numOfDatapoints, dimension, k,
+                     firstCentralIndexes, MAX_KMEANS_ITER);
     MyAssert(calcMat != NULL);
+    /* Convert result back to python type - tuple (LOL, List) */
     pyResult = kmeansResToPyObject(calcMat, k, dimension, numOfDatapoints);
 
     freeAllMemory();
     return pyResult;
 }
 
+/* The C-function that implements the Python function jacobi. */
 static PyObject *jacobi_connect(PyObject *self, PyObject *args) {
     PyObject *pyListOfLists, *pyResult;
     int i, n;
     double **eigenvectorsMat, **matrix;
-    headOfMemList= NULL, freeUsedMem = NULL;
+    headOfMemList= NULL, freeUsedMem = NULL; /* Init C memory containers */
 
     MyAssert(PyArg_ParseTuple(args, "Oi", &pyListOfLists, &n));
+    /* Assert fail == Type error - not in correct format */
+
+    /* Convert python types to C types */
     matrix = pyLOLToCMat(pyListOfLists, n, n);
     MyAssert(matrix != NULL);
+    /* Jacobi algorithm using 'jacobiAlgorithm' implementation in C */
     eigenvectorsMat = jacobiAlgorithm(matrix, n);
     MyAssert(eigenvectorsMat != NULL);
     for (i = 1; i < n; ++i) {
+        /* Order the eigenvalues list in the first row of the diag matrix */
         matrix[0][i] = matrix[i][i];
     }
+    /* Convert result back to python type - tuple (LOL, List) */
     pyResult = jacobiResToPyObject(eigenvectorsMat, matrix[0], n);
 
     freeAllMemory();
     return pyResult;
 }
 
+/***********************************
+** C <-> python convert functions **
+***********************************/
+
+/* This function Gets python int type list and convert it to C array. */
 int *pyIntListToCArray(PyObject *pyIntList, int len) {
     Py_ssize_t i;
     int *array, value;
 
     array = (int *) myAlloc(freeUsedMem, len * sizeof(int));
-    if (array != NULL) {
+    if (array != NULL) { /* Memory allocation fail */
         for (i = 0; i < len; ++i) {
             value = (int) PyLong_AsLong(PyList_GetItem(pyIntList, i));
             if (PyErr_Occurred()) {
@@ -156,15 +165,17 @@ int *pyIntListToCArray(PyObject *pyIntList, int len) {
     return array;
 }
 
+/* This function Gets C double array, build and return python type list (float). */
 PyObject *cArrToPythonList(double *array, int len) {
     Py_ssize_t i;
     PyObject *pyList, *pyValue;
+
     pyList = PyList_New(len);
-    if(pyList != NULL) {
+    if(pyList != NULL) { /* Memory allocation fail */
         for (i = 0; i < len; ++i) {
             pyValue = PyFloat_FromDouble(array[i]);
             if (pyValue == NULL || PyList_SetItem(pyList, i, pyValue)) {
-                Py_DecRef(pyList);
+                Py_DecRef(pyList); /* Free the list */
                 return NULL; /* Set error */
             }
         }
@@ -172,6 +183,7 @@ PyObject *cArrToPythonList(double *array, int len) {
     return pyList;
 }
 
+/* This function Gets python type list of lists (float) and convert into C double matrix. */
 double **pyLOLToCMat(PyObject *pyListOfLists, int rows, int cols) {
     Py_ssize_t i, j;
     double **matrix, value;
@@ -180,7 +192,7 @@ double **pyLOLToCMat(PyObject *pyListOfLists, int rows, int cols) {
     MyAssert(PyList_Check(pyListOfLists)); /* Is a list */
     /* Allocate memory for matrix */
     matrix = (double **) alloc2DArray(rows, cols, sizeof(double), sizeof(double *), freeUsedMem);
-    if (matrix != NULL) {
+    if (matrix != NULL) { /* Memory allocation fail */
         for (i = 0; i < rows; ++i) {
             pyList = PyList_GetItem(pyListOfLists, i);
             MyAssert(PyList_Check(pyList)); /* Is a list */
@@ -195,11 +207,12 @@ double **pyLOLToCMat(PyObject *pyListOfLists, int rows, int cols) {
     return matrix;
 }
 
+/* This function Gets C double array, build and return python type list (float). */
 PyObject *cMatToPyLOL(double **matrix, int rows, int cols) {
     Py_ssize_t i, j;
     PyObject *pyLOL, *pyList, *pyValue;
     pyLOL = PyList_New(rows);
-    if(pyLOL != NULL) {
+    if(pyLOL != NULL) { /* Memory allocation fail */
         for (i = 0; i < rows; ++i) {
             pyList = PyList_New(cols);
             if (pyList == NULL) {
@@ -223,6 +236,7 @@ PyObject *cMatToPyLOL(double **matrix, int rows, int cols) {
     return pyLOL;
 }
 
+/* This function pack kmeans results into python tuple. */
 PyObject *kmeansResToPyObject(double **matrix, int rows, int cols, int numOfDatapoints) {
     PyObject *pyCentroidsMat, *pyVecLabeling;
 
@@ -231,9 +245,11 @@ PyObject *kmeansResToPyObject(double **matrix, int rows, int cols, int numOfData
     if (pyCentroidsMat == NULL || pyVecLabeling == NULL)
         return NULL; /* Error */
 
+    /* Pack into tuple */
     return PyTuple_Pack(2, pyCentroidsMat, pyVecLabeling);
 }
 
+/* This function pack jacobi results into python tuple. */
 PyObject *jacobiResToPyObject(double **eigenvectorsMat, double *eigenvalues, int n) {
     PyObject *pyEigenvectorsMat, *pyEigenvalues;
 
@@ -242,5 +258,6 @@ PyObject *jacobiResToPyObject(double **eigenvectorsMat, double *eigenvalues, int
     if (pyEigenvectorsMat == NULL || pyEigenvalues == NULL)
         return NULL; /* Error */
 
+    /* Pack into tuple */
     return PyTuple_Pack(2, pyEigenvectorsMat, pyEigenvalues);
 }
